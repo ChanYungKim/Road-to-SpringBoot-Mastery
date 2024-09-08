25fa867c54a1c25c7059f0ce93b1924f005a8321
Kim Chan Yung <chanyung.kim96@gmail.com>
2024-09-08 23:15:29 +0800

    adding greetings

--- FirstProject/FirstProject/src/main/java/com/example/FirstProject/controller/FirstController.java
+++ FirstProject/FirstProject/src/main/java/com/example/FirstProject/controller/FirstController.java
@@ -1,10 +1,12 @@
 package com.example.FirstProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
+    @GetMapping("/greetings")
     public String nice2MeetYou () {
         return "greetings";
     }
