f50f1ed1b5bbd997431166377175f256fa46eab9
Kim Chan Yung <chanyung.kim96@gmail.com>
2024-09-09 00:58:31 +0800

    leaning model

--- FirstProject/FirstProject/src/main/java/com/example/FirstProject/controller/FirstController.java
+++ FirstProject/FirstProject/src/main/java/com/example/FirstProject/controller/FirstController.java
@@ -1,13 +1,15 @@
 package com.example.FirstProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/greetings")
-    public String nice2MeetYou () {
+    public String nice2MeetYou (Model model) {
+        model.addAttribute("username", "CYKC0");
         return "greetings";
     }
 
